9a6c7021803be675f76215ef365dfad85568873c
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-04-30 05:57:19 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// let numOne = 5;
+// let numTwo = 5;
 
-    console.log("Hello World");
-    console.log("Hello World2");
+function calc(numOne,numTwo) {
+    return console.log(
+        `
+        Suma: ${numOne}+${numTwo} = ${numOne+numTwo}
+        Resta: ${numOne}-${numTwo} = ${numOne-numTwo}
+        Multiplicacion: ${numOne}*${numTwo} = ${numOne*numTwo}
+        Division: ${numOne}/${numTwo} = ${numOne/numTwo}
+        `
+    )
+}
+calc(10,5)
+calc(5,5)
+calc(3,7)
