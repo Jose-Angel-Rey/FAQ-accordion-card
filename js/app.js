5701ff198d5b07bf74e161871a5db7f9795a257f
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-04-30 10:47:57 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
-const item = document.querySelectorAll(".accordion-list__item")
+const item = document.querySelectorAll(".accordion-list__item");
 
 console.log(item[0].children);
-item[0].addEventListener("click", ()=> {
-console.log(item[0].children);
-    
-})
+item[0].addEventListener("click", () => {
+  console.log(item[0].children);
+
+  console.log(item[0].children[1].classList);
+
+  return !item[0].children[1].classList.contains(
+    "accordion-list__item-description--visible"
+  )
+    ? item[0].children[1].classList.add(
+        "accordion-list__item-description--visible"
+      )
+    : item[0].children[1].classList.remove(
+        "accordion-list__item-description--visible"
+      );
+});
