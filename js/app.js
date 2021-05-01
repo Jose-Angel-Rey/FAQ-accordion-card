852f611550d45e84740f053ecee230d0e94f75a1
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-05-01 15:02:44 -0400

    Update

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const item = document.querySelectorAll(".accordion-list__item");
+const itemTitle = document.querySelectorAll(".accordion-list__item-title");
+const itemDesc = document.querySelectorAll(".accordion-list__item-description");
+const arrowIcon = document.querySelectorAll(".accordion-list__arrow-icon");
+
+console.log(item);
+console.log(item.length);
+
+for (let i = 0; i < item.length; i++) {
+  item[i].addEventListener("click", () => {
+    console.log(item[i].lastElementChild);
+    if (
+      !item[i].lastElementChild.classList.contains(
+        "accordion-list__item-description--visible"
+      )
+    ) {
+      itemTitle[i].classList.add("accordion-list__item-title--active");
+      itemDesc[i].classList.add("accordion-list__item-description--visible");
+      arrowIcon[i].classList.add("accordion-list__arrow-icon--rotate");
+    } else {
+      itemTitle[i].classList.remove("accordion-list__item-title--active");
+      itemDesc[i].classList.remove("accordion-list__item-description--visible");
+      arrowIcon[i].classList.remove("accordion-list__arrow-icon--rotate");
+    }
+  });
+}
+
+// function addEffects(){
+//   itemTitle.classList.add("accordion-list__item-title--active")
+//   itemDesc.classList.add("accordion-list__item-description--visible")
+//   arrowIcon.classList.add("accordion-list__arrow-icon--rotate")
+// }
+// function removeEffects(){
+//   itemTitle.classList.remove("accordion-list__item-title--active")
+//   itemDesc.classList.remove("accordion-list__item-description--visible")
+//   arrowIcon.classList.remove("accordion-list__arrow-icon--rotate")
+// }
